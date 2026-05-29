eb6d3d2a1e2a733993a7b7d0f9d2bac69d243861
kseniakopteva <ksenija.kopteva@gmail.com>
2026-05-29 13:13:00 +0300

    modals are now scrollable

--- app/components/base/BasicModal.jsx
+++ app/components/base/BasicModal.jsx
@@ -1,4 +1,4 @@
-import { Modal, Pressable } from "react-native";
+import { Modal, Pressable, ScrollView } from "react-native";
 import { sizes } from "../../../assets/theme";
 import BasicButton from "./BasicButton";
 
@@ -26,28 +26,32 @@ export default function BasicModal({
 				}}
 				onPress={onClose} // close modal
 			>
-				<Pressable
-					onPress={(e) => e.stopPropagation()} // prevent closing when inner content is clicked
-					style={{
-						backgroundColor: "white",
-						padding: sizes.modalPadding,
-						borderRadius: sizes.borderRadius,
-						margin: sizes.modalMargin,
-						position: "relative",
-						...style,
-					}}
-				>
-					{children}
-					{showCancelButton? (
-						<BasicButton
-							type="secondary"
-							style={{ marginTop: 10 }}
-							onPress={onClose}
-						>
-							Cancel
-						</BasicButton>
-					): <></>}
-				</Pressable>
+				<ScrollView>
+					<Pressable
+						onPress={(e) => e.stopPropagation()} // prevent closing when inner content is clicked
+						style={{
+							backgroundColor: "white",
+							padding: sizes.modalPadding,
+							borderRadius: sizes.borderRadius,
+							margin: sizes.modalMargin,
+							position: "relative",
+							...style,
+						}}
+					>
+						{children}
+						{showCancelButton ? (
+							<BasicButton
+								type="secondary"
+								style={{ marginTop: 10 }}
+								onPress={onClose}
+							>
+								Cancel
+							</BasicButton>
+						) : (
+							<></>
+						)}
+					</Pressable>
+				</ScrollView>
 			</Pressable>
 		</Modal>
 	);
